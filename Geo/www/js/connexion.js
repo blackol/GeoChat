16dd815f52dc0a6155c1a51e9c57c35846ac3d46
marmede <mehdidydy@hotmail.com>
2023-02-28 23:04:07 -0400

    connexion

--- Geo/www/js/connexion.js
+++ Geo/www/js/connexion.js
@@ -5,3 +5,24 @@ function onDeviceReady() {
   
 }
 
+function connect() {
+    var url = "http://212.227.202.166:8080/api/geotchatteurs/connexion/login?pseudo="+document.querySelector("input[name='pseudo']").value+"&mdp="+document.querySelector("input[name='mdp']").value;
+
+    fetch(url)
+    .then(function(response)
+    {
+        response.json().then(function(data) {
+            console.log(data);
+            if (data[0]){
+                window.location.assign("Carte.html");
+            }
+            else {
+                alert("Connexion échoué");
+            }
+        });
+    })
+    .catch(function(response)
+    {
+        console.log("pwoblem");
+    });
+}
