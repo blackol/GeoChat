6abc3ca0d1eaa016ec1dfedfafdb5979fb67492f
marmede <mehdidydy@hotmail.com>
2023-02-28 23:47:40 -0400

    protection si pseudo déjà existant

--- Geo/www/js/inscription.js
+++ Geo/www/js/inscription.js
@@ -16,43 +16,55 @@ function onSuccess(imageData) {
 }
 
 function submit() {
-    var url = "http://212.227.202.166:8080/api/geotchatteurs";
+    var url = "http://212.227.202.166:8080/api/geotchatteurs/pseudo/"+document.querySelector("input[name='pseudo']").value;
 
-    const formData  = new FormData();
-    formData.append("nom", document.querySelector("input[name='name']").value);
-    formData.append("prenom", document.querySelector("input[name='firstname']").value);
-    formData.append("sexe", document.querySelector("input[name='sexe']").value);
-    formData.append("pseudo", document.querySelector("input[name='pseudo']").value);
-    formData.append("mail", document.querySelector("input[name='mail']").value);
-    formData.append("mdp", document.querySelector("input[name='mdp']").value);
-    formData.append("dateNaissance", document.querySelector("input[name='birth']").value);
-    formData.append("photo", document.querySelector('input[name="photo"]').value);
-
-    const data = Object.fromEntries(formData);
-
-    var myInit = { method: 'POST',
-               headers: {
-                    'Content-type': 'application/json'
-               },
-               body: JSON.stringify(data),
-                };
-    
-    console.log(myInit);
-    fetch(url, myInit)
+    fetch(url)
     .then(function(response)
     {
         response.json().then(function(data) {
-            console.log(data);
             if(data.id) {
-                window.location.assign("Description.html");
+                alert('Pseudo déjà utilisé');
             }
             else {
-                alert("Une erreur est survenue lors de votre inscription.\nVérifiez que tous les champs sont bien remplis");
+                var url = "http://212.227.202.166:8080/api/geotchatteurs";
+                const formData  = new FormData();
+                formData.append("nom", document.querySelector("input[name='name']").value);
+                formData.append("prenom", document.querySelector("input[name='firstname']").value);
+                formData.append("sexe", document.querySelector("input[name='sexe']").value);
+                formData.append("pseudo", document.querySelector("input[name='pseudo']").value);
+                formData.append("mail", document.querySelector("input[name='mail']").value);
+                formData.append("mdp", document.querySelector("input[name='mdp']").value);
+                formData.append("dateNaissance", document.querySelector("input[name='birth']").value);
+                formData.append("photo", document.querySelector('input[name="photo"]').value);
+
+                const data = Object.fromEntries(formData);
+
+                var myInit = { method: 'POST',
+                        headers: {
+                                'Content-type': 'application/json'
+                        },
+                        body: JSON.stringify(data),
+                            };
+                
+                console.log(myInit);
+                fetch(url, myInit)
+                .then(function(response)
+                {
+                    response.json().then(function(data) {
+                        console.log(data);
+                        if(data.id) {
+                            window.location.assign("Description.html");
+                        }
+                        else {
+                            alert("Une erreur est survenue lors de votre inscription.\nVérifiez que tous les champs sont bien remplis");
+                        }
+                    });
+                })
+                .catch(function(response)
+                {
+                    console.log("pwoblem");
+                });
             }
         });
     })
-    .catch(function(response)
-    {
-        console.log("pwoblem");
-    });
 }
