62c45f31943d6cf2853a5184bcf1a0f45e8dd2d6
marmede <mehdidydy@hotmail.com>
2023-03-03 08:11:28 -0400

    last update

--- Geo/www/js/carte.js
+++ Geo/www/js/carte.js
@@ -6,6 +6,7 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
     //document.getElementById('btn').addEventListener("click", connect);
     getPosition();
+    getGeoTchatteurs();
 }
 
 var map = L.map("map").setView([16.2627584, -61.6628224], 13);
@@ -16,7 +17,7 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-var popup = L.popup()
+/*var popup = L.popup()
   .setLatLng([16.2627584, -61.6628224])
   .setContent(
     "<b>Hello world!</b><br>I am a popup. <Button>voir profil</Button>"
@@ -25,7 +26,7 @@ var popup = L.popup()
 
 function onMapClick() {
   alert("slatt");
-}
+}*/
 
 function getPosition() {
   var options = {
@@ -40,7 +41,7 @@ function getPosition() {
 
       var formData = new FormData();
 
-      formData.append('dateDernierreCollecte', new Date());
+      formData.append("dateDernierreCollecte", new Date().toLocaleDateString("en-CA"));
 
       var dataBis = Object.fromEntries(formData);
 
@@ -48,15 +49,16 @@ function getPosition() {
         method: 'PUT',
         headers: {
           'Content-type': 'application/json',
-          body: JSON.stringify(dataBis),
+          
         },
+        body: JSON.stringify(dataBis),
       };
 
       fetch(url, myInit)
       .then(function(response)
       {
         response.json().then(function(data){
-          alert(data);
+          
         })
       }).catch(function(response) {
         alert(response);
@@ -66,28 +68,31 @@ function getPosition() {
 
       formData = new FormData();
 
-      formData.append('derniereLatitude', position.coords.latitude);
-      formData.append('derniereLongitude', position.coords.longitude);
+      formData.append('derniereLatitude', parseFloat(position.coords.latitude));
+      formData.append('derniereLongitude', parseFloat(position.coords.longitude));
 
       dataBis = Object.fromEntries(formData);
 
       myInit = {
         method: 'PUT',
         headers: {
-          'Content-type': 'application/json',
-          body: JSON.stringify(dataBis),
+          'Content-type': 'application/json', 
         },
+        body: JSON.stringify(dataBis),
       }
 
       fetch(url, myInit)
       .then(function(response)
       {
         response.json().then(function(data){
-          alert(data);
+          
         })
       }).catch(function(response) {
         alert(response);
       });
+
+      localStorage.setItem('latitude', position.coords.latitude);
+      localStorage.setItem('longitude', position.coords.longitude);
 
       // move the map to have the location in its center
       window.map.panTo(new L.LatLng(position.coords.latitude, position.coords.longitude));
@@ -100,4 +105,91 @@ function getPosition() {
   var watchID = navigator.geolocation.getCurrentPosition(SuccessGeo, onError, options);
 }
 
-popup.on("click", onMapClick);
+function distance(x1, y1, x2, y2) {
+  const dx = x2 - x1;
+  const dy = y2 - y1;
+  const distance = Math.sqrt(dx*dx + dy*dy);
+  return distance;
+}
+
+function invite(id2) {
+  var url = "http://geotchat.blackol-ca.com:8080/api/invitations/";
+
+  const formData = new FormData();
+
+  formData.append("idinviteur", localStorage.getItem('id'));
+  formData.append("idinvite", parseInt(id2));
+  formData.append("dateinvitation", new Date().toLocaleDateString('en-CA'));
+  formData.append("idetat", 1);
+
+  const dataBis = Object.fromEntries(formData);
+
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json', 
+    },
+    body: JSON.stringify(dataBis)
+  })
+  .then(function(response)
+  {
+    response.json().then(function(data){
+      
+    })
+  })
+  .catch(function(response) {
+    alert("Error : "+ response);
+  });
+}
+
+function getGeoTchatteurs() {
+  var url = "http://geotchat.blackol-ca.com:8080/api/geotchatteurs/";
+
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json', 
+    },
+  })
+  .then(function(response)
+  {
+    response.json().then(function(data){
+      var tabMarker = [];
+
+      data.forEach(element => {
+        if(element.derniereLatitude != null && element.id != localStorage.getItem('id')) {
+          var dist = distance(localStorage.getItem('latitude'), element.derniereLongitude, localStorage.getItem('longitude'), element.derniereLatitude);
+          if(dist < 200.00) {
+            /* var popupI = new L.popup({
+              autoClose: false
+            })
+            .setLatLng([element.derniereLatitude, element.derniereLongitude])
+            .setContent(
+              "<b>Hello world!</b><br>I am a popup. en bal en fes <Button>voir profil</Button>"
+            )
+            .addTo(map);
+            alert('open like pince a linge');
+            pop2.on("click", onMapClick); */
+
+            let marker1= L.marker([element.derniereLatitude, element.derniereLongitude]).addTo(map);
+            marker1.bindPopup("<img src='"+element.photo +"' width='120px' />  <br> <b>Hello je m'appelle " + element.pseudo + "!</b>  <br> <p>Je suis née le "+element.dateNaissance +" </p> <button onclick='invite("+ element.id +")'>Inviter</button>");
+            tabMarker.push(marker1);
+
+
+          }
+        }
+         
+      });
+
+      for (e in tabMarker){
+        e.openPopup();
+      }
+    })
+  })
+  .catch(function(response) {
+    alert("Erreur : "+ response);
+  });
+}
+
+//popup.on("click", onMapClick);
---
+++ Geo/www/js/listecontact.js
@@ -0,0 +1,71 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+    //document.getElementById('btn').addEventListener("click", connect);
+}
+
+function loadInvit() {
+    var myId = localStorage.getItem('id');
+    var url = "http://geotchat.blackol-ca.com:8080/api/invitations/allofinvite?idinvite="+myId;
+
+    fetch(url)
+    .then(function(response)
+    {
+        response.json().then(function(data){
+            data.forEach(element => {
+
+                var url2 = "http://geotchat.blackol-ca.com:8080/api/geotchatteurs/"+element.idinviteur;
+                fetch(url2, {
+                    method: "GET"
+                })
+                .then(function(response2)
+                {
+                    response2.json().then(function(data2) {
+                        data2.forEach(element2 => {
+                            var div = document.createElement('div');
+                            div.setAttribute('class', 'matchinvitation');
+                            var img = document.createElement('img');
+                            img.setAttribute('class', 'photogeotahteur-invit');
+                            img.setAttribute('src', element2.photo);
+
+                            document.querySelector('#listcontenaire-invit').appendChild(div);
+
+                            var divInvit = document.createElement('div');
+                            divInvit.setAttribute('class', "TexteInvit");
+                            var span = document.createElement('span');
+                            span.innerHTML = element2.pseudo;
+                            divInvit.appendChild(span);
+                            div.appendChild(img);
+                            div.appendChild(divInvit);
+
+                            var button = document.createElement('button');
+                            button.innerHTML = "Voir le profil";
+                            divInvit = document.createElement('div');
+                            divInvit.setAttribute('class', "TexteInvit");
+                            divInvit.appendChild(button);
+
+                            divInvit = document.createElement('div');
+                            divInvit.setAttribute('class', "TexteInvit");
+                            button = document.createElement('button');
+                            button.innerHTML = "Accepter";
+
+                            divInvit.appendChild(button);
+                            button.innerHTML = "Refuser";
+                            divInvit.appendChild(button);
+                            div.appendChild(divInvit);
+                        });
+                    });
+                });
+                
+            });
+        }).catch(function(response) {
+            alert(response);
+        });
+
+
+        
+    });
+}
+
+loadInvit();
