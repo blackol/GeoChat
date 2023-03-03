b9a783795e5478690eea8adc4f6f060fbc5e6f16
marmede <mehdidydy@hotmail.com>
2023-03-02 22:37:44 -0400

    accueil update

--- Geo/www/js/pageaccueil.js
+++ Geo/www/js/pageaccueil.js
@@ -2,8 +2,8 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-    document.getElementById('connexion').addEventListener('click', connect, false);
-    document.getElementById('inscription').addEventListener('click', register, false);
+    //document.getElementById('connexion').addEventListener('click', connect, false);
+    //document.getElementById('inscription').addEventListener('click', register, false);
 }
 
 function connect() {
